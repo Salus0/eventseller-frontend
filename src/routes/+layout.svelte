9d298714e0b803c8060df475325dbb8e80cddb1a
Salus0 <s.ludwig.87@googlemail.com>
2026-08-03 20:47:38 +0200

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let data;
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/runs">Runs</a>
+  <a href="/items">Items</a>
+  <a href="/participants">Participants</a>
+  <a href="/auth/login">Login</a>
+</nav>
+
+<slot />
