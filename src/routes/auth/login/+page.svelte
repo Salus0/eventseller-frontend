4b009d46e89e2807ef8cdc66a63a8ee12a48aaf6
Salus0 <s.ludwig.87@googlemail.com>
2026-08-03 20:48:47 +0200

    Create +page.svelte

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  const backend = import.meta.env.PUBLIC_BACKEND_URL;
+  const discordUrl = `${backend}/auth/discord`;
+</script>
+
+<h1>Login</h1>
+
+<a href={discordUrl}>
+  <button>Mit Discord einloggen</button>
+</a>
