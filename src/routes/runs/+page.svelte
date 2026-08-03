9b4c3f8df1daba1f5e8a08eb61ee334f1dee1937
Salus0 <s.ludwig.87@googlemail.com>
2026-08-03 20:50:17 +0200

    Create +page.svelte

---
+++ src/routes/runs/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { api } from "$lib/api";
+
+  let runs = [];
+
+  api("/runs/test").then((r) => runs = r);
+</script>
+
+<h1>Runs</h1>
+
+<pre>{JSON.stringify(runs, null, 2)}</pre>
